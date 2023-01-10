0861ec28af1a2209be5005e39fb20d40638b41e0
GoldenBadger159 <mwallentine9291@gmail.com>
2023-01-10 11:36:01 -0700

    Finished prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        //Asks the users for first and last name then displays their input.
+        Console.WriteLine("What is your first name? ");
+        string FirstName = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        string LastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {FirstName} {LastName}");
     }
 }
